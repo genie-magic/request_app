ed8e41730eacb4897f8d43aac9bedfea4a9f28a0
happy-tinker <flashingtinker@gmail.comm>
2019-02-28 00:06:48 +0800

    refactor code

--- core/lib/src/redux/record_request/record_request.reducers.dart
+++ core/lib/src/redux/record_request/record_request.reducers.dart
@@ -3,7 +3,6 @@ import 'package:core/src/redux/record_request/record_request.state.dart';
 
 RecordRequestState recordRequestReducer(RecordRequestState state, dynamic action) {
   if (action is AddModeAction) {
-    print('add mode action is dispatched');
     return state.copyWith(
       bAddOrEdit: true,
       record: null,
